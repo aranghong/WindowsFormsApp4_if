6a6c88664be4133694a6f68a24a9ce0fc12fd361
aranghong <har2129@yu.ac.kr>
2025-04-29 13:27:25 +0900

    실습 동전던지기

--- WindowsFormsApp4_if/Form1.cs
+++ WindowsFormsApp4_if/Form1.cs
@@ -15,6 +15,38 @@ namespace WindowsFormsApp4_if
         public Form1()
         {
             InitializeComponent();
+
+            textBox1.Text = "입력: true \r\n";
+            if (Coin(true))
+            {
+                textBox1.Text += "승리";
+            }
+            else
+            {
+                textBox1.Text += "패배";
+            }
+
+        }
+
+        bool Coin(bool a)
+        {
+            Random random = new Random();
+            //int n = random.Next(100);   //0~99
+            int n = random.Next(1, 100);   //1~99
+            textBox1.Text += n + "\r\n";
+            int result = n % 2;
+
+            if (result == 0)
+            {
+                return false;
+            }
+            else if (result == 1)
+            {
+                return true;
+            }
+
+            return false;
+
         }
     }
 }

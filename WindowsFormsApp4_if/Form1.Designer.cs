6f527235d69651c159d698b515845da04c33e5ba
aranghong <har2129@yu.ac.kr>
2025-04-30 11:40:28 +0900

    사용자 입력, 라디오버튼 기능 추가

--- WindowsFormsApp4_if/Form1.Designer.cs
+++ WindowsFormsApp4_if/Form1.Designer.cs
@@ -29,21 +29,83 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(113, 56);
+            this.textBox1.Location = new System.Drawing.Point(113, 70);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(531, 331);
+            this.textBox1.Size = new System.Drawing.Size(558, 117);
             this.textBox1.TabIndex = 0;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(249, 28);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(52, 19);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.Text = "true";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(444, 27);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(59, 19);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.Text = "false";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox2.Location = new System.Drawing.Point(113, 212);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(549, 139);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(690, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 108);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "input";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(41, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -55,6 +117,11 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- WindowsFormsApp4_if/Form1.cs
+++ WindowsFormsApp4_if/Form1.cs
@@ -12,19 +12,23 @@ namespace WindowsFormsApp4_if
 {
     public partial class Form1 : Form
     {
+        string inputData = "";
+
         public Form1()
         {
             InitializeComponent();
 
-            textBox1.Text = "입력: true \r\n";
-            if (Coin(true))
-            {
-                textBox1.Text += "승리";
-            }
-            else
-            {
-                textBox1.Text += "패배";
-            }
+            //string inputData = "";
+
+            //textBox1.Text = "입력: " + inputData + "\r\n";
+            //if (Coin(true))
+            //{
+            //    textBox1.Text += "승리";
+            //}
+            //else
+            //{
+            //    textBox1.Text += "패배";
+            //}
 
         }
 
@@ -33,7 +37,7 @@ namespace WindowsFormsApp4_if
             Random random = new Random();
             //int n = random.Next(100);   //0~99
             int n = random.Next(1, 100);   //1~99
-            textBox1.Text += n + "\r\n";
+            textBox2.Text += n + "\r\n";
             int result = n % 2;
 
             if (result == 0)
@@ -47,6 +51,60 @@ namespace WindowsFormsApp4_if
 
             return false;
 
+        }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            // 실제 처리 내용 (없으면 비워둬도 OK)
+
+            if (inputData.Equals("") && radioButton1.Checked) {
+                textBox1.Text = "true";
+                textBox2.Text = "";
+            }
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (inputData.Equals("") && radioButton2.Checked)
+            {
+                textBox1.Text = "false";
+                textBox2.Text = "";
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            inputData = textBox1.Text;
+
+            if (inputData.Equals("true") || inputData.Equals("false"))
+            {
+                textBox2.Text = "입력: " + inputData + "\r\n";
+                if (Coin(true))
+                {
+                    textBox2.Text += "승리";
+                }
+                else
+                {
+                    textBox2.Text += "패배";
+                }
+            }
+            else
+            {
+                textBox2.Text = "true, false 중 입력하세요! \r\n";
+            }
+
+            inputData = "";
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            textBox1.Text = "";
+            textBox2.Text = "";
+            inputData = "";
         }
     }
 }
